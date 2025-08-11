6b297a2f1dd5c430bbf6655f5bc4d14662665205
Vidushi Sharma <vidushi.sh12@gmail.com>
2025-08-11 16:25:53 +0530

    Updated app.js for deplyment.

--- backend/app.js
+++ backend/app.js
@@ -2,29 +2,34 @@ const express = require("express");
 const app = express();
 const router = require("./router/auth-router.js");
 const connectDb = require("./utils/db.js");
-const errorMiddleware = require("./middlewares/error-middleware.js")
+const errorMiddleware = require("./middlewares/error-middleware.js");
 const cors = require("cors");
 const taskRouter = require("./router/task-router.js");
+const path = require("path");
 
 const corsOptions = {
-	origin:"http://localhost:5173",
-	methods:"GET, POST, PUT, DELETE, PATCH, HEAD",
-	credentials:true,
-}
-
+  origin: "http://localhost:5173",
+  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+  credentials: true,
+};
 app.use(cors(corsOptions));
 
 app.use(express.json());
-
 
 app.use("/api/auth", router);
 app.use("/api/data", taskRouter);
 
 app.use(errorMiddleware);
 
+app.use(express.static(path.join(__dirname, "../frontend/task-wise/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/task-wise/dist/index.html"));
+});
+
 const PORT = process.env.PORT || 5174;
 connectDb().then(() => {
-	app.listen(PORT, () => {
-		console.log(`Server is running on port ${PORT}`);
-	});
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
